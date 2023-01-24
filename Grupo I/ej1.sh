f26ee6d273c505a050f218c6f6f4527e068b1795
xaplhu <97669917+xaplhu@users.noreply.github.com>
2023-01-24 15:48:11 +0100

    movido fichero a grupo 1

---
+++ Grupo I/ej1.sh
@@ -0,0 +1,7 @@
+
+echo "Ingresa tu nombre: "
+read nombre
+echo "Ingresa tus dos apellidos: "
+read apellido1 apellido2
+sleep 5
+echo "Tu nombre completo es $nombre $apellido1 $apellido2"
--- ej1.sh
+++
@@ -1,7 +0,0 @@
-
-echo "Ingresa tu nombre: "
-read nombre
-echo "Ingresa tus dos apellidos: "
-read apellido1 apellido2
-sleep 5
-echo "Tu nombre completo es $nombre $apellido1 $apellido2"
