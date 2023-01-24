90d81475ad9cd87d9b76ce1a6db06b127b8833f0
xaplhu <97669917+xaplhu@users.noreply.github.com>
2023-01-24 16:16:13 +0100

    Creados ficheros del ej3 al ej6 y script del ej3

---
+++ ej3.sh
@@ -0,0 +1,19 @@
+$sum=0;
+$count=0;
+
+while true; do
+  echo "Introduce un valor: ";
+  read value;
+  if [ $value -eq 0 ]; then
+    break
+  fi
+  $sum=$((sum + value));
+  $count=$((count + 1));
+done
+
+if [ $count -eq 0 ]; then
+  echo "No se ha introducido ningún valor.";
+else
+  echo "La suma total de valores introducidos es $sum.";
+  echo "El valor medio es $(($sum / $count)).";
+fi
