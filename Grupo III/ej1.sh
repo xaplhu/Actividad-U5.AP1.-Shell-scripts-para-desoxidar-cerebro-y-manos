8029ad0c5524d11608cb6b7be96acc1603c3953c
xaplhu <97669917+xaplhu@users.noreply.github.com>
2023-01-24 17:35:11 +0100

    Modificado fichero haciendo script

---
+++ Grupo III/ej1.sh
@@ -0,0 +1,12 @@
+echo "Ingrese el primer número:";
+read $num1;
+echo "Ingrese el segundo número:";
+read $num2;
+
+if [ $num1 -gt $num2 ]; then
+  echo "El primer número es mayor.";
+elif [ $num2 -gt $num1 ]; then
+  echo "El segundo número es mayor.";
+else
+  echo "Los dos números son iguales.";
+fi
