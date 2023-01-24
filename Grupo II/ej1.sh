892baeedd00e2675fe8c92bd7a84fe752b1cd646
xaplhu <97669917+xaplhu@users.noreply.github.com>
2023-01-24 16:17:19 +0100

    Cambios fichero ejs1 y 2 del Grupo II

--- Grupo II/ej1.sh
+++ Grupo II/ej1.sh
@@ -1,9 +1,9 @@
-echo "Introduce la primera cadena: "
-read cadena1
-echo "Introduce la segunda cadena: "
-read cadena2
+echo "Introduce la primera cadena: ";
+read cadena1;
+echo "Introduce la segunda cadena: ";
+read cadena2;
 if [ "$cadena1" == "$cadena2" ]; then
-  echo "Las cadenas son iguales."
+  echo "Las cadenas son iguales.";
 else
-  echo "Las cadenas son diferentes."
+  echo "Las cadenas son diferentes.";
 fi
--- Grupo II/ej2.sh
+++ Grupo II/ej2.sh
@@ -1,11 +1,11 @@
 if [ $# -eq 0 ] || [ $1 -le 0 ]; then
   echo "Uso: $0 valor"
-  echo "El script debe recibir como argumento un valor mayor que 0."
-  exit 1
+  echo "El script debe recibir como argumento un valor mayor que 0.";
+  exit 1;
 fi
 
 if [ $(($1 % 2)) -eq 0 ]; then
-  echo "El valor introducido es par."
+  echo "El valor introducido es par.";
 else
-  echo "El valor introducido es impar."
+  echo "El valor introducido es impar.";
 fi
